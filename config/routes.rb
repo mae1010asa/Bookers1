6d7c25d05cdb1d5c600a294df5bf815a33a2756d
Ubuntu <ubuntu@ip-172-31-26-254.ap-northeast-1.compute.internal>
2025-09-12 09:11:59 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'books/new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "homes#top"
+  resources :books
+
+end
